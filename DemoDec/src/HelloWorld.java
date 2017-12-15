262c1fb950e192421a07f06312f78b19a361c8e6
Hema G <hema.g@capgemini.com>
2017-12-15 08:10:12 +0530

    c4

--- DemoDec/src/HelloWorld.java
+++ DemoDec/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		for(int i=0;i<10;i++)
-			System.out.println("Auto Build "+i);
+			System.out.println("Friday - 15 Dec "+i);
 
 	}
 
