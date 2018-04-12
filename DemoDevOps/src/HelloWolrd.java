69da6890b6443ef4627445f7ebd924d06146d8a8
Hema G <hema.g@capgemini.com>
2018-04-12 16:48:04 +0530

    ThisWorks

--- DemoDevOps/src/HelloWolrd.java
+++ DemoDevOps/src/HelloWolrd.java
@@ -2,6 +2,7 @@
 public class HelloWolrd {
 
 	public static void main(String[] args) {
+		System.out.println("***********Welcome***************");
 		for(int i=1;i<10;i++)
 			System.out.println("GIT "+i);
 		System.out.println("***********Thank You***************");
