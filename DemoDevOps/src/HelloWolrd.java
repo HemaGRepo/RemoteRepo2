3c8b4a98e0918bf14e9edb89359136637d74929f
Hema G <hema.g@capgemini.com>
2018-04-13 14:57:58 +0530

    Copyright data

--- DemoDevOps/src/HelloWolrd.java
+++ DemoDevOps/src/HelloWolrd.java
@@ -7,6 +7,8 @@ public class HelloWolrd {
 		int i=0;
 		for(i=0;i<=10;i++)
 			System.out.println("Iteration..."+i);
+		for(i=0;i<5;i++)
+			System.out.println("Copyright  Capgemini....."+i);
 
 	}
 
