d350aef28aa979357c0af18bf3dc77f1d4eed085
Hema G <hema.g@capgemini.com>
2018-04-13 14:54:51 +0530

    Pipeline Commit

--- DemoDevOps/src/HelloWolrd.java
+++ DemoDevOps/src/HelloWolrd.java
@@ -4,6 +4,9 @@ public class HelloWolrd {
 	public static void main(String[] args) {
 		System.out.println("DevOps");
 		System.out.println("This is new code...");
+		int i=0;
+		for(i=0;i<=10;i++)
+			System.out.println("Iteration..."+i);
 
 	}
 
